5720970f3e35e40f06dd86b754ed67c569e1018f
patricia.cosma <p.n.cosma@gmail.com>
2022-09-02 18:42:06 +0300

    added CSS and JS

---
+++ advent-calendar/script.js
@@ -0,0 +1,5 @@
+const button = document.querySelector('.btn-start');
+const container = document.querySelector ('.container');
+
+// const days - to be able to loop through the days in the calendar
+const days = 24;
